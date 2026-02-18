3381698fce33f41afa2cf7b6105fd2ab64f009fe
Adim2P <miguellicauco@gmail.com>
2026-02-18 16:57:21 +0800

    Extracted Dashboard Function from Main

---
+++ src/pitherm/dashboard.py
@@ -0,0 +1,19 @@
+import requests
+from src.pitherm.config import THINGSPEAK_API_KEY
+
+def send_to_thingspeak(temp, hum):
+    url = "https://api.thingspeak.com/update"
+    params = {
+        'api_key': THINGSPEAK_API_KEY,
+        'field1': temp,
+        'field2': hum,
+    }
+
+    try:
+        r = requests.get(url, params=params)
+        if r.status_code == 200 and r.text !='0':
+            print("[UPLOAD] Data sent to ThingSpeak.")
+        else:
+            print(f"[WARN] ThingSpeak error: {r.text}")
+    except Exception as err:
+        print("[ERROR] ThingSpeak exception:", err)
