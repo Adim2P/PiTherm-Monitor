ba0cf71965272fa8addcc093a1ea6c6c4291afe4
Adim2P <miguellicauco@gmail.com>
2026-04-10 10:23:04 +0800

    Rewritten from scratch, part of making SMTP Modular

--- src/pitherm/alert.py
+++ src/pitherm/alert.py
@@ -1,50 +1,14 @@
-import requests
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-
-from src.pitherm.config import (
-    SMTP_USER,
-    SMTP_PASS,
-    SMTP_RECIPIENT,
-    SMTP_CC
-)
-
-def build_recipients(primary):
-    cc_list = [e.strip() for e in SMTP_CC.split(",")] if SMTP_CC else []
-    return primary, cc_list, [primary] + cc_list
+from src.pitherm.smtp_newClient import SMTPClient
 
 def send_email_alert(temp, hum, alert_type="high"):
-    smtp_host = 'smtp2.agmstech.com'
-    smtp_port = 587
-    sender = SMTP_USER
+    subject = "ALERT: High Temperature" if alert_type == "high" else "ALERT: Low temperature"
 
-    primary_to, cc_list, recipients = build_recipients(SMTP_RECIPIENT)
-    subject = "ALERT: High temperature" if alert_type == "high" else "ALERT: Low temperature"
-    
     body = f"""
-    Server Room Temperature Alert:
+Server Room Temperature Alert:
 
-    Temperature: {temp:.1f}°C
-    Humidity: {hum:.1f}%
+Temperature: {temp:.1f}°C
+Humidity: {hum:.1f}%
 
-    - Raspberry Pi Temperature Monitor
-    """
-    msg = MIMEMultipart()
-    msg['From'] = sender
-    msg['To'] = primary_to
-    if cc_list:
-        msg['Cc'] = ", ".join(cc_list)
-    msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
-
-    try:
-        server = smtplib.SMTP(smtp_host, smtp_port, timeout=5)
-        server.starttls()
-        server.login(sender, SMTP_PASS)
-        server.sendmail(sender, recipients, msg.as_string())
-        server.quit()
-        print("[OK] Email alert sent via SMTP.")
-    except Exception as err:
-        print("[ERROR] Failed to send email:", err)
-
+- Raspberry Pi Temperature Monitor
+"""
+    SMTPClient().send(subject, body)
