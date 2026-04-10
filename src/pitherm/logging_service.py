e4f058ee779f022640ea6fc540bf7e8e7b457ca4
Adim2P <miguellicauco@gmail.com>
2026-04-10 17:12:53 +0800

    Removed SMTP Logic from Logging, Part of SMTP Modularity

--- src/pitherm/logging_service.py
+++ src/pitherm/logging_service.py
@@ -5,16 +5,8 @@ from datetime import datetime
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from src.pitherm.smtp_client import SMTPClient
 from email.mime.application import MIMEApplication
-from src.pitherm.config import (
-    SMTP_USER,
-    SMTP_PASS,
-    SMTP_RECIPIENT,
-    SMTP_CC
-)
 import csv
 
 _last_report_month = None
@@ -70,10 +62,6 @@ def archive_old_logs():
                     os.rename(src_path, dst_path)
                     print(f"[ARCHIVE] Moved {file} to archive.")
 
-def build_recipients(primary):
-    cc_list = [e.strip() for e in SMTP_CC.split(",")] if SMTP_CC else []
-    return primary, cc_list, [primary] + cc_list
-
 def log_to_excel(temp, hum):
     ensure_log_directories()
     
@@ -115,38 +103,32 @@ def send_monthly_report():
     if not os.path.exists(filename):
         print("[WARN] No Excel File to send.")
         return
-    
-    sender = SMTP_USER
-    primary_to, cc_list, recipients = build_recipients(SMTP_RECIPIENT)
 
     subject = f"Monthly Temp Report - {month_str}"
-    body = f"Attached is the temperature and humidity log for {month_str}.\n\n- Raspberry Pi Monitor"
-
-    msg = MIMEMultipart()
-    msg['From'] = sender
-    msg['To'] = primary_to
-    if cc_list:
-        msg['Cc'] = ", ".join(cc_list)
-    msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
+    body = f"""
+    <p>Attached is the temperature and humidity log for {month_str}.</p>
+    <p>- Raspberry Pi Monitor</p>
+    """
 
     with open(filename, 'rb') as f:
         attachment = MIMEApplication(
             f.read(),
             _subtype='vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         )
-        attachment.add_header('Content-Disposition', 'attachment', filename=filename)
-        msg.attach(attachment)
+        attachment.add_header(
+            'Content-Disposition',
+            'attachment',
+            filename=filename
+        )
     
-    try:
-        server = smtplib.SMTP('smtp2.agmstech.com', 587)
-        server.starttls()
-        server.login(sender, SMTP_PASS)
-        server.sendmail(sender, recipients, msg.as_string())
-        server.quit()
-        print("[OK] Monthly Excel report sent via email.")
-    except Exception as err:
-        print("[ERROR] Failed to send excel report:", err)
+    SMTPClient().send(
+        subject, 
+        body, 
+        is_html=True, 
+        attachment=attachment
+    )
+
+    return body #temporary
 
 def check_and_send_monthly_report():
     global _last_report_month
