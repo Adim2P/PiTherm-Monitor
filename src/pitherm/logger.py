0d72865f69283e5959a7599b28199e30a04f0f6c
Adim2P <miguellicauco@gmail.com>
2026-06-08 20:51:55 +0800

    Add: Logging Feature for Backtracking

---
+++ src/pitherm/logger.py
@@ -0,0 +1,27 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+logger = logging.getLogger("pitherm")
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    "[%(asctime)s] [%(levelname)s] %(message)s"
+)
+
+file_handler = RotatingFileHandler(
+    os.path.join(LOG_DIR, "runtime.log"),
+    maxBytes=5_000_000,
+    backupCount=5
+)
+
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
