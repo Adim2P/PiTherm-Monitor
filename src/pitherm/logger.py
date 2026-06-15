739a23f0f0fb99bd93b4ffd0e34e729af8f5b737
Adim2P <miguellicauco@gmail.com>
2026-06-15 18:32:19 +0800

    Fix: Possible Duplication Issue on Runtime

--- src/pitherm/logger.py
+++ src/pitherm/logger.py
@@ -6,22 +6,24 @@ LOG_DIR = "logs"
 os.makedirs(LOG_DIR, exist_ok=True)
 
 logger = logging.getLogger("pitherm")
-logger.setLevel(logging.INFO)
 
-formatter = logging.Formatter(
-    "[%(asctime)s] [%(levelname)s] %(message)s"
-)
+if not logger.handlers:
+    logger.setLevel(logging.INFO)
 
-file_handler = RotatingFileHandler(
-    os.path.join(LOG_DIR, "runtime.log"),
-    maxBytes=5_000_000,
-    backupCount=5
-)
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] %(message)s"
+    )
 
-file_handler.setFormatter(formatter)
+    file_handler = RotatingFileHandler(
+        os.path.join(LOG_DIR, "runtime.log"),
+        maxBytes=5_000_000,
+        backupCount=5
+    )
 
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
 
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
